e5ad029e9cc5c3aa36ba0c8ff797613af7629a47
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-07-06 01:32:17 +0530

    created server.ts file to start the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,3 +2,43 @@ import mongoose from "mongoose";
 import z from "zod";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import express from "express";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import { database } from "./config/db";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/content", contentRouter);
+
+
+app.use(errroHandler);
+
+
+const startSever = async () => {
+    await database.connect();
+
+    if (!database.isConnected()) {
+        return;
+    }
+
+    app.listen(port, () => console.log(`Server Started at port: ${port}`));
+}
+
+
+if (process.argv[1] === __filename) {
+    startSever();
+}
