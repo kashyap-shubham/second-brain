78f8113b1521cd943d09e27f210229ddb0b7cac8
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-09-15 00:36:15 +0530

    refactor to class based validator

--- server/src/middlewares/validator.ts
+++ server/src/middlewares/validator.ts
@@ -3,22 +3,24 @@ import { AnyZodObject, ZodError } from "zod";
 import { ApiError } from "../utils/apiError";
 import { HttpStatus } from "../constants/httpStatus";
 
-export const validate =
-  (schema: AnyZodObject) =>
-  (req: Request, res: Response, next: NextFunction): void => {
-    try {
-      schema.parse({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
-      next();
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const errors = err.errors.map((e) => e.message).join(", ");
-        next(new ApiError(errors, HttpStatus.BAD_REQUEST));
-      } else {
-        next(err);
+export class Validator {
+  public static validate(schema: AnyZodObject) {
+    return (req: Request, res: Response, next: NextFunction): void => {
+      try {
+        schema.parse({
+          body: req.body,
+          query: req.query,
+          params: req.params,
+        });
+        next();
+      } catch (err) {
+        if (err instanceof ZodError) {
+          const errors = err.errors.map((e) => e.message).join(", ");
+          next(new ApiError(errors, HttpStatus.BAD_REQUEST));
+        } else {
+          next(err);
+        }
       }
-    }
-  };
+    };
+  }
+}
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { UserController} from "../controllers/user.controllers";
-import { authMiddleware } from "../middlewares/auth.middleware";
+import { Validator } from "../middlewares/validator";
+import { signinSchema, signupSchema } from "../schema/user.schema";
 
 const userRouter = express.Router();
 
@@ -11,12 +12,12 @@ const userController = new UserController(userService);
 
 // @route  POST /api/v1/user/signup
 // @desc signup new user
-userRouter.route("/signup").post(validate(signupSchema), userController.signup);
+userRouter.route("/signup").post(Validator.validate(signupSchema), userController.signup);
 
 
 // @route Post /api/v1/user/sigin
 // @desc sinin registered users
-userRouter.route("/signin").post(validate(signinSchema), userController.signin)
+userRouter.route("/signin").post(Validator.validate(signinSchema), userController.signin)
 
 
 
